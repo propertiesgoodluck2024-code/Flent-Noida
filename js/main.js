952cf6cc82279d5c58159f41cf28071088c6c28b
propertiesgoodluck2024-code <propertiesgoodluck2024@gmail.com>
2025-11-17 13:22:02 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+/ small behaviors: mobile toggle & dynamic year
+document.getElementById('year').textContent = new Date().getFullYear();
+const mobileToggle = document.getElementById('mobileToggle');
+mobileToggle.addEventListener('click', () => {
+  const nav = document.querySelector('.nav');
+  nav.style.display = nav.style.display === 'block' ? '' : 'block';
+});
+document.getElementById('callBtn').addEventListener('click', () => {
+  window.location.href = 'tel:+91 9315381500, 9582505055, 9990444451';
+});
 
